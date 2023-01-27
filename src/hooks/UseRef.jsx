a8b559d67260c69a3f0aae3f7945bd393360ed1c
mrcruz117 <mrcruz1989@gmail.com>
2023-01-27 13:36:38 -0500

    completed UseRef

--- src/hooks/UseRef.jsx
+++ src/hooks/UseRef.jsx
@@ -3,25 +3,23 @@ import React, { useEffect, useRef, useState } from "react";
 export default function UseRef() {
   const [name, setName] = useState("");
   const renderCount = useRef(1);
-  const inputRef = useRef();
+  const prevName = useRef("");
   // {current: 0}
 
   useEffect(() => {
-    renderCount.current = renderCount.current + 1;
-  });
+    prevName.current = name;
+  }, [name]);
 
   function focus() {
-    inputRef.current.focus();
+    prevName.current.focus();
   }
 
   return (
     <>
-      <input
-        ref={inputRef}
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-      />
-      <div>My name is {name}</div>
+      <input value={name} onChange={(e) => setName(e.target.value)} />
+      <div>
+        My name is {name} and it used to be {prevName.current}
+      </div>
       <button onClick={focus}>Focus</button>
     </>
   );
