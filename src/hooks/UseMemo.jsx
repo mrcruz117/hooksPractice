3a74400a353a7029d6ff1711b490040cc2b9fb7f
mrcruz117 <mrcruz1989@gmail.com>
2023-01-31 14:10:50 -0500

    UseMemo init

---
+++ src/hooks/UseMemo.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+export default function UseMemo() {
+  const [number, setNumber] = React.useState(0);
+  const [dark, setDark] = React.useState(false);
+  const doubleNumber = slowFunction(number);
+  const themeStyles = {
+    backgroundColor: dark ? "#333" : "#FFF",
+    color: dark ? "#FFF" : "#333",
+  };
+
+  return (
+    <>
+      <input
+        type="number"
+        value={number}
+        onChange={(e) => setNumber(parseInt(e.target.value))}
+      />
+      <button onClick={() => setDark((prevDark) => !prevDark)}>
+        Toggle Theme
+      </button>
+      <div style={themeStyles}>{doubleNumber}</div>
+    </>
+  );
+}
+
+function slowFunction(num) {
+  console.log("Calling slow function");
+  for (let i = 0; i <= 1000000000; i++) {}
+  return num * 2;
+}
