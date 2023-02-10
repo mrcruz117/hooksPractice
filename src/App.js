e797feae7db495440e15ca4f4b8caf5a675cc1df
mrcruz117 <mrcruz1989@gmail.com>
2023-02-10 06:22:40 -0500

    added new use effect in useLocalStorage

--- src/App.js
+++ src/App.js
@@ -2,11 +2,17 @@ import UseCallback from "./hooks/UseCallback";
 import UseMemo from "./hooks/UseMemo";
 import UseReducer from "./hooks/UseReducer";
 import UseRef from "./hooks/UseRef";
+import useLocalStorage from "./hooks/customHooks/useLocalStorage";
 
 function App() {
+  const [name, setName] = useLocalStorage("name", "");
   return (
     <>
-      <UseReducer />
+      <input
+        type={"text"}
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
     </>
   );
 }
