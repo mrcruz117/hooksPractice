07f798b92ba38d0e7d6fee37251ef56e4a98612b
mrcruz117 <mrcruz1989@gmail.com>
2023-02-15 10:00:58 -0500

    created useTimeout

--- src/App.js
+++ src/App.js
@@ -5,13 +5,15 @@ import UseRef from "./hooks/UseRef";
 import useLocalStorage from "./hooks/customHooks/useLocalStorage";
 import useUpdateLogger from "./hooks/customHooks/useUpdateLogger";
 import ToggleComponent from "./testComponents/ToggleComponent";
+import TimeoutComponent from "./testComponents/TimeoutComponent";
 
 function App() {
   const [name, setName] = useLocalStorage("name", "");
   useUpdateLogger(name);
   return (
     <>
-      <ToggleComponent />
+      {/* <ToggleComponent /> */}
+      <TimeoutComponent />
     </>
   );
 }
