0fe36d2aa6f9bb389d0aa18adc0b70255e12732a
mrcruz117 <mrcruz1989@gmail.com>
2023-02-15 09:27:43 -0500

    completed useToggle

--- src/App.js
+++ src/App.js
@@ -4,17 +4,14 @@ import UseReducer from "./hooks/UseReducer";
 import UseRef from "./hooks/UseRef";
 import useLocalStorage from "./hooks/customHooks/useLocalStorage";
 import useUpdateLogger from "./hooks/customHooks/useUpdateLogger";
+import ToggleComponent from "./testComponents/ToggleComponent";
 
 function App() {
   const [name, setName] = useLocalStorage("name", "");
   useUpdateLogger(name);
   return (
     <>
-      <input
-        type={"text"}
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-      />
+      <ToggleComponent />
     </>
   );
 }
